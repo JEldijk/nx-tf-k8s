2271a3c9e8ba5dd821495fc945774f2873a41571
JEldijk <j.eldijk@gmail.com>
2023-03-08 21:24:40 +0100

    feat(lib-loggin): added get errorMessage and toErrorwithmessage

--- libs/logging/src/lib/logging.service.ts
+++ libs/logging/src/lib/logging.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ErrorWithMessage, isErrorWithMessage } from './ErrorWithMessage';
 
 @Injectable()
-export class LoggingService {}
+export class LoggingService {
+  public toErrorWithMessage = (maybeError: unknown): ErrorWithMessage => {
+    if (isErrorWithMessage(maybeError)) return maybeError;
+
+    try {
+      return new Error(JSON.stringify(maybeError));
+    } catch {
+      // fallback in case there's an error stringifying the maybeError
+      // like with circular references for example.
+      return new Error(String(maybeError));
+    }
+  };
+
+  public getErrorMessage = (error: unknown) => {
+    return this.toErrorWithMessage(error).message;
+  };
+}
